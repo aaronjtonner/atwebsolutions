d96f0bdad062235749266ec8a908e7be572b282b
Aaron Tonner <tonneraaron@gmail.com>
2021-07-29 12:41:17 -0400

    add footer links

--- src/components/footers/footerBasic.js
+++ src/components/footers/footerBasic.js
@@ -2,12 +2,44 @@ import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components"
-import { Container } from "../layoutComponents"
+import { Container, Flex } from "../layoutComponents"
+import { FaPhoneAlt, FaRegClock } from "react-icons/fa"
+import { SiMailDotRu } from "react-icons/si"
+import { IoLocationSharp } from "react-icons/io5"
+import { AnchorInline, ButtonInline } from "../buttons"
 
 const FooterWrapper = styled.footer`
   background: var(--clr-primary);
   color: var(--txt-light);
   padding: 7em 0 2em 0;
+`
+
+const FlexContact = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > * + * {
+    margin-left: 5px;
+  }
+
+  a {
+    color: var(--clr-accent);
+  }
+`
+
+const FlexItem = styled.div`
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: var(--spacer) 0;
+
+    li {
+      a {
+        color: var(--txt-light);
+        font-weight: var(--fw-500);
+      }
+    }
+  }
 `
 
 const Socials = styled.div`
@@ -57,6 +89,75 @@ export default function FooterBasic() {
               />
             </center>
           </Logo>
+          <Flex>
+            <FlexItem>
+              <p className="heading bold upper">get in touch</p>
+              <ul>
+                <li>
+                  <FlexContact>
+                    <FaPhoneAlt className="accent" />
+                    <a className=" italics" href="tel: 587-437-9161">
+                      587-437-9161
+                    </a>
+                  </FlexContact>
+                </li>
+                <li>
+                  <FlexContact>
+                    <SiMailDotRu className="accent" />
+                    <a className="italics" href="mailto: aaron@aarontonner.com">
+                      aaron@aarontonner.com
+                    </a>
+                  </FlexContact>
+                </li>
+                <li>
+                  <FlexContact>
+                    <FaRegClock className="accent" />
+                    <p>9:00 AM - 4:00 PM</p>
+                  </FlexContact>
+                </li>
+              </ul>
+              <ul>
+                <li>Calgary, AB</li>
+                <li>Canada</li>
+              </ul>
+            </FlexItem>
+            <FlexItem>
+              <p className="heading bold upper">website</p>
+              <ul>
+                <li>
+                  <StyledLink to="/">home</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/portfolio">portfolio</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/services">services</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/services/web-designer-calgary">
+                    web designer calgary
+                  </StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/contact">contact</StyledLink>
+                </li>
+              </ul>
+            </FlexItem>
+            <FlexItem className="spacing">
+              <p className="heading bold upper">About</p>
+              <ul>
+                <li>
+                  I offer affordable{" "}
+                  <ButtonInline to="/services/calgary-web-designer">
+                    web design
+                  </ButtonInline>
+                  , web development, regular web maintenance & updates, local
+                  SEO, and other internet marketing services in Calgary, AB and
+                  surrounding areas.
+                </li>
+              </ul>
+            </FlexItem>
+          </Flex>
           <Socials>
             <a target="_popup" href="https://www.linkedin.com/in/aarontonner/">
               <StaticImage
